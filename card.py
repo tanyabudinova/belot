d0f958e9c1b2447fe2728c14200446ffb8f432c8
antonio <antonio.g.georgiev1998@gmail.com>
2020-03-25 23:55:28 +0200

    Editted the names of the files.

---
+++ card.py
@@ -0,0 +1,4 @@
+class Card():
+	def __init__(self, rank, suite):
+		self.rank = rank
+		self.suite = suite
---
+++ suites.py
@@ -0,0 +1,6 @@
+from enum import Enum 
+class Suite(Enum):
+	Clubs = 1 
+	Diamonds = 2
+	Hearts = 3
+	Spades = 4
