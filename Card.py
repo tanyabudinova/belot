dad22b041733e69bda8680744f6171653e1d6a65
antonio <antonio.g.georgiev1998@gmail.com>
2020-03-25 23:57:00 +0200

    Deleted the old files.

--- Card.py
+++
@@ -1,4 +0,0 @@
-class Card():
-	def __init__(self, rank, suite):
-		self.rank = rank
-		self.suite = suite
--- Suites.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum 
-class Suite(Enum):
-	Clubs = 1 
-	Diamonds = 2
-	Hearts = 3
-	Spades = 4
