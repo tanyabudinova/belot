6ae7838c03d9f67f1cc5113b810bcfcfaee48227
antonio <antonio.g.georgiev1998@gmail.com>
2020-03-25 23:34:01 +0200

    Added classes Card, Rank, Suite

---
+++ Ranks.py
@@ -0,0 +1,6 @@
+from enum import Enum 
+class Rank(Enum):
+	Clubs = 1 
+	Diamonds = 2
+	Hearts = 3
+	Spades = 4
---
+++ Suites.py
@@ -0,0 +1,10 @@
+from enum import Enum 
+class Suite(Enum):
+	Seven = 7
+	Eight = 8
+	Nine = 9
+	Ten = 10
+	Jack = 11
+	Queen = 12
+	King = 13
+	Ace = 14
