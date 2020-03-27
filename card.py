c6dfeb76bb8d488fe12eafbf4945bf37bcdc57c8
antonio <antonio.g.georgiev1998@gmail.com>
2020-03-27 14:35:30 +0200

    Errors in the process of handling.

--- card.py
+++ card.py
@@ -3,8 +3,8 @@ from suits import Suit
 
 class Card():
     def __init__(self, rank, suit):
-        self.rank = Rank(rank)
-        self.suit = Suit(suit)
+        self.rank = rank
+        self.suit = suit
 
     def __gt__(self, other):
     	if self.suit == other.suit:
@@ -15,4 +15,4 @@ class Card():
         return f"{self.rank}{self.suit}"
 
     def __eq__(self, other):
-    	return (self.suit == other.suit) and (self.rank == other.rank)
+    	return self.suit == other.suit and self.rank == other.rank
